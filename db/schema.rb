8eb3189fd0890c0a0ba491b63bb45a29f7af546e
Al Snow <jasnow@hotmail.com>
2023-02-26 19:16:09 -0500

    Upgraded Rails to latest edge version; reset db

--- db/schema.rb
+++ db/schema.rb
@@ -3,16 +3,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_003739) do
-
+ActiveRecord::Schema[7.1].define(version: 2019_06_20_003739) do
   create_table "spell_books", force: :cascade do |t|
     t.string "name"
     t.integer "wizard_id"
@@ -23,8 +22,8 @@ ActiveRecord::Schema.define(version: 2019_06_20_003739) do
     t.integer "wizard_id", null: false
     t.string "wood_type"
     t.integer "core_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["wizard_id"], name: "index_wands_on_wizard_id"
   end
 
@@ -33,8 +32,8 @@ ActiveRecord::Schema.define(version: 2019_06_20_003739) do
     t.integer "house"
     t.string "parent_email"
     t.text "notes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
